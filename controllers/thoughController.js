66b0417a9458dccb081467c6a166b53f8f588987
Nestor <you@example.com>
2023-06-22 20:14:47 -0400

    added thoughtcontroller.js and usercontroller.js

---
+++ controllers/thoughController.js
@@ -0,0 +1,61 @@
+const { User, Thought } = require("../models")
+
+const thoughtController = {
+  getThought(req, res) {
+    Thought.find({})
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No user found with this id" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, New: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No thoughts were found with the current id" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thoughts were found with the current id" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'Thought was deleted, but no user was found'})
+          : res.json({ message: 'Thought was deleted successfully' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
+module.exports = thoughtController
